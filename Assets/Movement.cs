3ba45af4b2c4cd3a7d25158fe99273f38a3940e4
Tasos <tasostsouk.tt@gmail.com>
2016-09-17 14:37:29 +0200

    Main stuff

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody m_rigidbody;
+    Vector3 vel = new Vector3(0,0,100);
+
+    // Use this for initialization
+    void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_rigidbody.velocity = vel;
+    }
+}
