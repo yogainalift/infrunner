61843805132e0442d88e836e70449246e15320ad
chris t <ctselepidas@gmail.com>
2016-09-30 18:24:18 +0200

    Basic movement lerping

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,35 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Movement : MonoBehaviour {
 
-	[SerializeField]
-	private float lerpSpeed;
+    [SerializeField]
+    public float lerpSpeed;
     
 	Rigidbody m_rigidbody;
-    Vector3 vel = new Vector3(0,0,50);
+    private readonly Vector3 _vel = new Vector3(0,0,50);
 
-	Vector3 oldPos;
-	Vector3 newPos;
+	Vector3 oldPosition;
+	private Vector3 newPosition;
 
+    void Awake() {
+        newPosition = transform.position;
+    }
+    
     // Use this for initialization
     void Start () {
         m_rigidbody = GetComponent<Rigidbody>();
-		newPos = new Vector3(oldPos.x +2.7f, oldPos.y,oldPos.z);
-		oldPos = m_rigidbody.transform.position;
+		newPosition = new Vector3(oldPosition.x +2.7f, oldPosition.y,oldPosition.z);
+		oldPosition = m_rigidbody.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        m_rigidbody.velocity = vel;
+//        m_rigidbody.velocity = _vel;
 	
-		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			m_rigidbody.MovePosition(new Vector3(this.m_rigidbody.position.x + 2.8f, this.m_rigidbody.position.y, this.m_rigidbody.position.z)); 
-			//m_rigidbody.transform.position = Vector3.Lerp(oldPos, newPos, Time.deltaTime * 3);
-		}
-		else if(Input.GetKeyDown (KeyCode.LeftArrow)) {
-			m_rigidbody.MovePosition(new Vector3(this.m_rigidbody.position.x - 2.8f, this.m_rigidbody.position.y, this.m_rigidbody.position.z)); 
-			//Vector3 goLeft = new Vector3(m_rigidbody.position.x - 2.7f, m_rigidbody.position.y, m_rigidbody.position.z);
-		}
+
+        PositionChanging();
 	}
+
+    void PositionChanging()
+    {
+        Vector3 pos = transform.position;
+        Vector3 positionA = new Vector3( -2.7f, pos.y, pos.z );
+        Vector3 positionB = new Vector3(0, pos.y, pos.z);
+        Vector3 positionC = new Vector3( pos.x + 2.7f, pos.y, pos.z );
+
+        if ( Input.GetKeyDown( KeyCode.Q ) && pos.x <= 0 ) {
+            newPosition = positionA;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && pos.x > 0)
+        {
+            newPosition = positionB;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            newPosition = positionB;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * lerpSpeed);
+    }
 }
