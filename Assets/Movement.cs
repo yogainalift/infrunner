92098985ea27455524bbe48ec32173d345f4c616
chris t <ctselepidas@gmail.com>
2016-10-02 21:50:42 +0200

    fixed right-left movement and speed script

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,65 +4,83 @@ using System;
 
 public class Movement : MonoBehaviour {
 
-    [SerializeField]
+
+    //The Z(forward)-velocity of the player
+    public float myVelocity;
+
+    //The speed of the left right movement
     public float lerpSpeed;
 
+    //The x distance for left-right positions
+    private static float AMMOUNT = 2.7f;
 
-        enum POS
+    //enum that holds possible positions
+    public enum POS
         {
-            POSA,
-            POSB,
-            POSC
+            LEFT,
+            CENTER,
+            RIGHT
         };
 
-    
-	Rigidbody m_rigidbody;
-    private readonly Vector3 _vel = new Vector3(0,0,50);
 
+    //initial position
+    public POS myPos = POS.CENTER;
+
+    //the new position if there is change
 	private Vector3 newPosition;
 
     void Awake() {
         newPosition = transform.position;
     }
     
-    // Use this for initialization
     void Start () {
-        m_rigidbody = GetComponent<Rigidbody>();
-
     }
 	
-	// Update is called once per frame
 	void Update () {
-        //m_rigidbody.velocity = _vel;
-	    
-
+        //calling the right left movement procedure
         PositionChanging();
-	}
+    }
 
+    //holds the logic for the player movement
     void PositionChanging()
     {
+
+        //pos variable for usability
         Vector3 pos = transform.position;
-        Vector3 positionA = new Vector3( -2.7f, pos.y, pos.z );
-        Vector3 positionB = new Vector3(0.01f, pos.y, pos.z);
-        Vector3 positionC = new Vector3( 2.7f, pos.y, pos.z );
 
-        if ( Input.GetKeyDown( KeyCode.Q ) && pos.x <= 0.02f )
-        {
-            newPosition = positionA;
-        }
-        else if (Input.GetKeyDown(KeyCode.Q) && pos.x > 0.00f )
-        {
-            newPosition = positionB;
-        }
-        else if ( Input.GetKeyDown( KeyCode.E ) && pos.x >= 0.00f ) {
-            newPosition = positionC;
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && pos.x < 0.00f )
-        {
-            newPosition = positionB;
-        }
+        //possible positions (this can probably be improved)
+        Vector3 positionA = new Vector3( -2.7f, pos.y, pos.z);
+        Vector3 positionB = new Vector3(0.0f, pos.y, pos.z);
+        Vector3 positionC = new Vector3( 2.7f, pos.y, pos.z);
 
 
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * lerpSpeed);
+        //detecting input and making changes accordingly
+        if ( Input.GetKeyDown( KeyCode.E ) ) {
+            if (myPos == POS.LEFT)
+            {
+                myPos = POS.CENTER;
+                newPosition = positionB;
+            }
+            else
+            {
+                myPos = POS.RIGHT;
+                newPosition = positionC;
+            }
+        }
+        else if ( Input.GetKeyDown( KeyCode.Q ) ) {
+            if (myPos == POS.RIGHT)
+            {
+                myPos = POS.CENTER;
+                newPosition = positionB;
+            }
+            else
+            {
+                myPos = POS.LEFT;
+                newPosition = positionA;
+            }
+        }
+
+        //finaly perform the lerp and give player speed
+        transform.position = Vector3.Lerp(transform.position, new Vector3(newPosition.x, pos.y, transform.position.z + (myVelocity / lerpSpeed) ), Time.deltaTime * lerpSpeed);
     }
 }
